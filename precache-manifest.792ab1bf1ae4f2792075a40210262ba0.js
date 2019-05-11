524b9421d757af7edc93717a40a7b4e8d3db3b2b
tanjuha <tanya.oleksandrivna19.07@gmail.com>
2019-05-11 21:53:51 +0300

    Updates

---
+++ precache-manifest.792ab1bf1ae4f2792075a40210262ba0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8f34b5e6ac427a951027509be87b1ea",
+    "url": "/homework_react-basics/index.html"
+  },
+  {
+    "revision": "891a51bbb5e901400dc2",
+    "url": "/homework_react-basics/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/homework_react-basics/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "891a51bbb5e901400dc2",
+    "url": "/homework_react-basics/static/js/main.a9346f24.chunk.js"
+  },
+  {
+    "revision": "d5f26ccb5d792a591a75",
+    "url": "/homework_react-basics/static/js/runtime~main.afe325dc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/homework_react-basics/static/media/logo.5d5d9eef.svg"
+  }
+]);
